c9704f12f1c28174320439d8c5bf1efc6e75a531
Kaustav Banerjee <kaustav@opencraft.com>
2025-08-27 19:31:04 +0530

    chore: remove registration tab

--- src/logistration/Logistration.jsx
+++ src/logistration/Logistration.jsx
@@ -125,7 +125,7 @@ const Logistration = (props) => {
                 )
                 : (!isValidTpaHint() && !hideRegistrationLink && (
                   <Tabs defaultActiveKey={selectedPage} id="controlled-tab" onSelect={(tabKey) => handleOnSelect(tabKey, selectedPage)}>
-                    <Tab title={formatMessage(messages['logistration.register'])} eventKey={REGISTER_PAGE} />
+                    {/* <Tab title={formatMessage(messages['logistration.register'])} eventKey={REGISTER_PAGE} /> */}
                     <Tab title={formatMessage(messages['logistration.sign.in'])} eventKey={LOGIN_PAGE} />
                   </Tabs>
                 ))}
